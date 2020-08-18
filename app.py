568794d62eff67cf4107285614375c7b8e6c4053
jtrinidad6 <jtrinidad6@dxc.com>
2020-08-17 23:50:58 -0400

    Added additional models and ouput
    added lsa, luhn, lex summaries.

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 from flask import Flask, render_template, request
 from flask_material import Material
 import json
-from sumy.parsers.plaintext import PlaintextParser
-from sumy.nlp.tokenizers import Tokenizer
-from sumy.summarizers.lsa import LsaSummarizer
+import summarizers
 
 app = Flask(__name__)
 Material(app)
@@ -13,24 +11,34 @@ def index():
     return render_template("index.html")
 
 
+
+
 @app.route('/summarize', methods=['POST'])
 def summarize():
     if request.method == 'POST':
         user_input = request.form['user_input']
-        # Latent Semantic Analysis is an unsupervised method of summarization.
-        # It combines term frequency techniques with singular value decomposition to summarize texts.
+
         
-        full_lsa = ""
-        sentence_cnt = 5
-        parser = PlaintextParser(user_input, Tokenizer("english"))
+        #LSA_Summary
+        lsa_summary = summarizers.lsa_summary(user_input, num_sentences_out = 10)
 
-        model_lsa = LsaSummarizer()
-        result_lsa = model_lsa(parser.document, sentence_cnt)
-        for i in result_lsa:
-            full_lsa = full_lsa + str(i)
-        #print(full_lsa)
+        #luhn_summary
+        luhn_summary = summarizers.luhn_summary(user_input, num_sentences_out = 10)
 
-        sum_result =  {'user_input': user_input, "summary": full_lsa}
+        #LEX_Summary
+        lex_summary = summarizers.lex_summary(user_input, num_sentences_out = 10)
+
+
+
+
+        #RESULTS
+        sum_result =  {"user_input": user_input, 
+                        "summaries": {
+                            "lsa_summary": lsa_summary, 
+                            "luhn_summary": luhn_summary, 
+                            "lex_summary": lex_summary
+                            } 
+                        }
 
         return render_template("index.html", results = sum_result)
        
--- summarizers.py
+++ summarizers.py
@@ -1,7 +1,69 @@
-from summarizer import Summarizer
+#bert_summarize
+import requests
+#from summarizer import Summarizer
+from newspaper import fulltext
+#lsa_summary
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer
+from sumy.summarizers.lsa import LsaSummarizer
+#luhn_summary
+from sumy.summarizers.luhn import LuhnSummarizer
+#lex_summary
+from sumy.summarizers.lex_rank import LexRankSummarizer 
 
-text = "run"
-model = Summarizer()
-result = model(text)
-full = "".join(result)
-print(full)
+from validator_collection import validators, checkers
+
+# def bert_summarize(user_input, num_sentences_out = 10):
+#     model = Summarizer()
+
+#     if checkers.is_url('not a valid url'):
+#         user_input = fulltext(requests.get(user_input).text)
+    
+#     result = model(user_input, num_sentences = num_sentences_out - 1)
+#     summary = "".join(result)
+
+#     return summary
+
+def lsa_summary(user_input, num_sentences_out = 10):
+    # Latent Semantic Analysis is an unsupervised method of summarization.
+    # It combines term frequency techniques with singular value decomposition to summarize texts.
+
+    if checkers.is_url(user_input):
+        user_input = fulltext(requests.get(user_input).text)
+
+    full_lsa = ""
+    parser = PlaintextParser(user_input, Tokenizer("english"))
+
+    model_lsa = LsaSummarizer()
+    result_lsa = model_lsa(parser.document, num_sentences_out)
+    for i in result_lsa:
+        full_lsa = full_lsa + str(i)
+
+    return full_lsa
+
+def luhn_summary(user_input, num_sentences_out = 10):
+    if checkers.is_url(user_input):
+        user_input = fulltext(requests.get(user_input).text)
+
+    summary_luhn = ""
+    parser = PlaintextParser(user_input, Tokenizer("english"))
+
+    model_luhn = LuhnSummarizer()
+    result_luhn = model_luhn(parser.document, num_sentences_out)
+    for i in result_luhn:
+        summary_luhn = summary_luhn + str(i) + " "
+        
+    return summary_luhn
+
+def lex_summary(user_input, num_sentences_out = 10):
+    if checkers.is_url(user_input):
+        user_input = fulltext(requests.get(user_input).text)
+
+    summary_lex = ""
+    parser = PlaintextParser(user_input, Tokenizer("english"))
+
+    model_lex = LexRankSummarizer()
+    result_lex = model_lex(parser.document, num_sentences_out)
+    for i in result_lex:
+        summary_lex = summary_lex + str(i) + " "
+    return summary_lex
