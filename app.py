153a8d6fb51acc765b800fe270f3eb972b09205f
jtrinidad6 <jtrinidad6@dxc.com>
2020-08-18 04:40:57 -0400

    disabled cache in flask

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
 from flask import Flask, render_template, request
-from flask_material import Material
 import json
 import summarizers
 
 app = Flask(__name__)
-Material(app)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
 
 @app.route("/")
 def index():
